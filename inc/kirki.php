afd059c500f0f4556e0c24955ce86b84dd323d24
aroy15 <anjonroy57@gmail.com>
2023-10-30 03:12:09 +0600

    Kirki repeater field added on customizer section

--- inc/kirki.php
+++ inc/kirki.php
@@ -30,6 +30,58 @@ Kirki::add_field('customizer_section_one', array(
     'priority' => 10,
 ));
 
+Kirki::add_field('customizer_section_one', array(
+    'type'     => 'repeater',
+    'settings' => 'repeater_setting',
+    'label'    => esc_html__( 'Repeater Control', 'customizer' ),
+    'section'  => 'customizer_section_one',
+    'priority' => 10,
+    'default'  => [
+        [
+            'link_text'   => esc_html__( 'Kirki Site', 'customizer' ),
+            'link_url'    => 'https://kirki.org/',
+            'link_target' => '_self',
+            'checkbox'    => false,
+        ],
+        [
+            'link_text'   => esc_html__( 'Kirki WP', 'customizer' ),
+            'link_url'    => 'https://wordpress.org/plugins/kirki/',
+            'link_target' => '_blank',
+            'checkbox'    => true,
+        ],
+    ],
+    'fields'   => [
+        'link_text'   => [
+            'type'        => 'text',
+            'label'       => esc_html__( 'Link Text', 'customizer' ),
+            'description' => esc_html__( 'Description', 'customizer' ),
+            'default'     => '',
+        ],
+        'link_url'    => [
+            'type'        => 'text',
+            'label'       => esc_html__( 'Link URL', 'customizer' ),
+            'description' => esc_html__( 'Description', 'customizer' ),
+            'default'     => '',
+        ],
+        'link_target' => [
+            'type'        => 'select',
+            'label'       => esc_html__( 'Link Target', 'customizer' ),
+            'description' => esc_html__( 'Description', 'customizer' ),
+            'default'     => '_self',
+            'choices'     => [
+                '_blank' => esc_html__( 'New Window', 'customizer' ),
+                '_self'  => esc_html__( 'Same Frame', 'customizer' ),
+            ],
+        ],
+        'checkbox'    => [
+            'type'    => 'checkbox',
+            'label'   => esc_html__( 'Checkbox', 'customizer' ),
+            'default' => false,
+        ],
+    ],
+));
+
+
 // another section under 'About Panel'
 Kirki::add_section('customizer_section_two', array(
     'title' => esc_attr__('About Section', 'customizer'),
